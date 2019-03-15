09c199a33df5009630e0326f42b2c2a7ca17a3cf
firastahir <33678277+firastahir@users.noreply.github.com>
2019-03-15 12:25:40 -0500

    Update myscr.tf

--- myscr.tf
+++ myscr.tf
@@ -1,5 +1,5 @@
-provider "google" {
+//provider "google" {
  // credentials = "${file("./serviceaccount.json")}"
-  project     = "composed-strata-222317"
-  region      = "us-central1"
-}
+ // project     = "composed-strata-222317"
+ // region      = "us-central1"
+//}
